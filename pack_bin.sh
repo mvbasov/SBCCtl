5871eb292b670a79552b973a38dba1c3e1830e5e
Mikhail Basov (NeedleFile) <mikhail[at]basov[dot]net>
2023-04-26 04:22:27 +0300

    Automate bin archive version numbering

--- pack_bin.sh
+++ pack_bin.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 cd bin
-zip SBCCtl_$(git describe --tags --abbrev=0 HEAD)_$(git rev-parse --short HEAD).zip *.bin flash.sh
+last_tag=$(git describe --tags --abbrev=0 HEAD)
+commit_count=$(git rev-list --count ${last_tag}..HEAD)
+current_commit=$(git rev-parse --short HEAD)
+if [[ ${commit_count} -eq "0" ]]; then
+  bin_version="${last_tag}-${current_commit}"
+else
+  bin_version="${last_tag}-${commit_count}-${current_commit}"
+fi
+zip SBCCtl_${bin_version}.zip *.bin flash.sh
 cd -
